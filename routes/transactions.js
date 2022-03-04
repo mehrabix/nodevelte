3c82f79bf8715b4be8f499c22ca631c6f8bfd881
mehrabix <mehrabi@post.com>
2022-03-05 00:44:45 +0330

    remove comments

--- routes/transactions.js
+++ routes/transactions.js
@@ -4,37 +4,16 @@ const Transactions = require("../models/Transactions");
 const router = Router();
 
 router.get("/", async (req, res) => {
-  // try {
-  //   const transactions = await Transactions.find();
-  //   if (!transactions) {
-  //     throw new Error("no transactions found");
-  //   }
-  //   res.status(200).json(transactions);
-  // } catch (err) {
-  //   res.status(400).json({ message: err.message });
-  // }
+
   Transactions.find().then(result => {
     res.status(200).json(result);
   }).catch(err => {
     res.status(400).json({ message: err.message });
   })
 
-
 });
-
 router.post("/", async (req, res) => {
-  // const { value, data } = req.body;
-  // const newtransaction = new Transactions({ value, data });
 
-  // try {
-  //   const transaction = await newtransaction.save();
-  //   if (!transaction) {
-  //     throw new Error("no transaction found");
-  //   }
-  //   res.status(200).json(transaction);
-  // } catch (err) {
-  //   res.status(500).json({ message: err.message });
-  // }
     const { value, data } = req.body;
     const newtransaction = new Transactions({ value, data });
     newtransaction.save()
@@ -44,7 +23,7 @@ router.post("/", async (req, res) => {
       .catch(err => {
         res.status(500).json({ message: err.message });
       });
-
+0
 });
 
 module.exports = router;
