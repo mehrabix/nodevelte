f896ff8131a9b3f6a8234755c0c45ded379770de
mehrabix <mehrabi@post.com>
2022-03-16 11:42:37 +0330

    delete api

--- routes/transactions.js
+++ routes/transactions.js
@@ -14,16 +14,32 @@ router.get("/", async (req, res) => {
 });
 router.post("/", async (req, res) => {
 
-    const { value, data } = req.body;
-    const newtransaction = new Transactions({ value, data });
-    newtransaction.save()
-      .then(result => {
-        res.status(200).json(result);
-      })
-      .catch(err => {
-        res.status(500).json({ message: err.message });
-      });
+  const { value, data } = req.body;
+  const transaction = new Transactions({ value, data });
+  transaction.save()
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      res.status(500).json({ message: err.message });
+    });
 
 });
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params
+  const transaction = Transactions.findById(id)
+  if (!transaction) {
+    res.status(404).json({ message: 'transaction not found' })
+  }
+  transaction.remove()
+    .then(result => {
+      res.status(200).json(result)
+    })
+    .catch(err => {
+      res.status(500).json({ message: err.message })
+    })
+})
+
 
 module.exports = router;
