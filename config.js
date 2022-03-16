eb0c41ebb7a67101f561e6cba28b9db46039fda2
mehrabix <mehrabi@post.com>
2022-03-16 11:54:39 +0330

    change port

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 5000,
     mongoURI: 'mongodb+srv://dbmehrabi:tD3GcOLzCIkLuXBv@cluster0.vj2l3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 }
