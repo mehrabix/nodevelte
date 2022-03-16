5569ef7faf2f461eafe8e81d9ab4eaa3c52689b0
mehrabix <mehrabi@post.com>
2022-03-16 12:39:27 +0330

    vite config with proxy

---
+++ frontend/src/App.svelte
@@ -0,0 +1,1 @@
+<h1>Not yet something</h1>
--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,10 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+import sveltePreprocess from 'svelte-preprocess'
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter()
-	}
-};
-
-export default config;
+export default {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: sveltePreprocess()
+}
