7b977cdf5556970577d66399b2500ee4ff725290
mehrabix <mehrabi@post.com>
2022-03-20 12:27:54 +0330

    wd

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,6 +3,8 @@
     import "./app.css";
     let transactions = [];
     let inputValue = "";
+    let valueDeleted = false;
+    let valueAdded = false;
     $: isButtonDisabled = inputValue.length === 0;
     onMount(() => {
         fetch("/api/transactions")
@@ -28,6 +30,8 @@
                     transactions = data;
                 });
                 inputValue = "";
+                valueAdded = true;
+                valueDeleted = false;
         });
     };
     const deleteValue = (id) => {
@@ -42,16 +46,18 @@
                 .then((data) => {
                     transactions = data;
                 });
+                valueAdded = false;
+                valueDeleted = true;
         });
     };
 </script>
 
-<div class="flex justify-center flex-col items-center h-screen">
+<div dir="rtl" class="flex justify-center flex-col items-center h-screen">
     <input
         class="input input-bordered w-full max-w-xs"
         bind:value={inputValue}
         type="text"
-        placeholder="something..."
+        placeholder="عدد بنویس..."
     />
     <button disabled={isButtonDisabled} class="btn btn-success mt-5" on:click={addValue}>add</button>
     {#each transactions as t}
@@ -59,8 +65,25 @@
             <div class="badge badge-primary">
                 {t.value}
             </div>
-            <button class="btn btn-error" on:click={() => deleteValue(t._id)}>delete</button
+            <button class="btn btn-error" on:click={() => deleteValue(t._id)}>حذف</button
             >
         </div>
     {/each}
+    {#if valueAdded && setTimeout(() => { valueAdded = false }, 3000)}
+    <div class="alert alert-success shadow-lg w-3/5 mt-10">
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+          <span>مقدار اضافه شد</span>
+        </div>
+      </div>
+    {/if}
+      {#if valueDeleted && setTimeout(() => { valueDeleted = false }, 3000)}
+      <div class="alert alert-error shadow-lg w-3/5 mt-10">
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+          <span>مقدار حذف شد</span>
+        </div>
+      </div>
+      {/if}
+      
 </div>
